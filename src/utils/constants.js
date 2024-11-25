84881b8a7a74888f5836ba2918ae6bef3f023285
swatiSahu07 <30427069+swatiSahu07@users.noreply.github.com>
2024-11-25 11:15:01 +0530

    Create constants.js

---
+++ src/utils/constants.js
@@ -0,0 +1,9 @@
+export const CDN_URL = "https://media-assets.swiggy.com/swiggy/";
+
+export const LOGO_URL =
+  "https://i.pinimg.com/564x/b1/fc/bb/b1fcbbfd4fb8116c714ef352bb39bbaf.jpg";
+
+export const RES_API =
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.37240&lng=78.43780&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+
+  
